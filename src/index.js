49d59e0e00d3281d7f90773112cbc08eb604d871
Alexandra <arbeuerman@gmail.com>
2021-05-10 16:26:11 -0500

    adding new button

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ fetch(`${recipesUrl}/limit=${pageSize}/offset=${pageOffset}`)
 
 
 function displayRecipes(recipes){
-    recipes.forEach (recipe => 
-        {
-            const recipeDiv = document.createElement('span')
-            recipeDiv.innerHTML = `${recipe.name}`
-            const recipeBar = document.getElementById('recipe-bar')
-            recipeBar.append(recipeDiv)
-        })
+    recipes.forEach (recipe => displayRecipe(recipe));
+}
 
+function displayRecipe(recipe)
+{
+    const recipeDiv = document.createElement('span');
+    recipeDiv.innerHTML = `
+        <h4>${recipe.name}</h4>
+        <img src=${recipe.image}>
+        `;
+    const recipeBar = document.getElementById('recipe-bar');
+    recipeBar.append(recipeDiv);
 }
