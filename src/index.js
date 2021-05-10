4cd52e4481fa9271720a696c41376d641a88a17c
Alexandra <arbeuerman@gmail.com>
2021-05-10 15:40:44 -0500

    some small changes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ const recipesUrl = "http://localhost:3000/recipes"
 const pageSize = 5;
 let currentPage = 1;
 
-fetch(`${recipesUrl}/?_page=${currentPage}&_limit=${pageSize}`) 
+fetch(`${recipesUrl}/?_page=${currentPage}&_limit=${pageSize}`)
 .then(res=>res.json())
-.then(displayRecipes)
+.then(displayRecipes);
+
 
 function displayRecipes(recipes){
-console.log(recipes)
-recipes.forEach (recipe => {
-    const recipeDiv = document.createElement('span')
-    recipeDiv.innerHTML = `${recipe.name}`
-    const recipeBar = document.getElementById('recipe-bar')
-    recipeBar.append(recipeDiv)
-})
+    recipes.forEach (recipe => 
+        {
+            const recipeDiv = document.createElement('span')
+            recipeDiv.innerHTML = `${recipe.name}`
+            const recipeBar = document.getElementById('recipe-bar')
+            recipeBar.append(recipeDiv)
+        })
 
 }
