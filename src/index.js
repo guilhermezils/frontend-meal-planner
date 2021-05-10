e394b4f3689bd754af82117b33f0d5d9d7dae4eb
Alexandra <arbeuerman@gmail.com>
2021-05-10 17:07:44 -0500

    adding button functionality for paging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const recipesUrl = "http://localhost:3000/recipes"
 const pageSize = 5;
 let pageOffset = 0;
+const recipeBar = document.getElementById('recipe-bar');
 
-fetch(`${recipesUrl}/limit=${pageSize}/offset=${pageOffset}`) 
-.then(res=>res.json())
-.then(displayRecipes);
+getRecipes();
 
+function getRecipes() 
+{
+    recipeBar.innerHTML = '';
+
+    fetch(`${recipesUrl}/limit=${pageSize}/offset=${pageOffset}`) 
+    .then(res=>res.json())
+    .then(displayRecipes);
+}
 
 function displayRecipes(recipes){
     recipes.forEach (recipe => displayRecipe(recipe));
@@ -18,6 +25,19 @@ function displayRecipe(recipe)
         <h4>${recipe.name}</h4>
         <img src=${recipe.image}>
         `;
-    const recipeBar = document.getElementById('recipe-bar');
     recipeBar.append(recipeDiv);
+}
+
+//get button element to add page updates to 
+document.getElementById('next-page').addEventListener('click', () => updatePage(5))
+document.getElementById('previous-page').addEventListener('click', () => updatePage(-5))
+
+function updatePage(value)
+{
+    pageOffset = pageOffset + value;
+    if(pageOffset < 0)
+    {
+        pageOffset = 0;
+    }
+    getRecipes();
 }
