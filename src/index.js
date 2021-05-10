f5844603d986af3d38f6b67ab9ab3e9a44740d7d
Alexandra <arbeuerman@gmail.com>
2021-05-10 16:06:15 -0500

    fixing url to paginate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const recipesUrl = "http://localhost:3000/recipes"
 const pageSize = 5;
-let currentPage = 1;
+let pageOffset = 0;
 
-fetch(`${recipesUrl}/?_page=${currentPage}&_limit=${pageSize}`) 
+fetch(`${recipesUrl}/limit=${pageSize}/offset=${pageOffset}`) 
 .then(res=>res.json())
 .then(displayRecipes)
 
