63e1dc847445bfd425e4235ad7d64cbdf636dc1a
Alexandra <arbeuerman@gmail.com>
2021-05-10 16:09:27 -0500

    update url

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ const recipesUrl = "http://localhost:3000/recipes"
 const pageSize = 5;
 let pageOffset = 0;
 
-<<<<<<< HEAD
-fetch(`${recipesUrl}/?_page=${currentPage}&_limit=${pageSize}`)
-=======
 fetch(`${recipesUrl}/limit=${pageSize}/offset=${pageOffset}`) 
->>>>>>> get-recipes
 .then(res=>res.json())
 .then(displayRecipes);
 
