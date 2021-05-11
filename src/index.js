1479b4861f490cc1a5096f8a179f8f6367487b2a
Alexandra <arbeuerman@gmail.com>
2021-05-11 14:08:32 -0500

    adding meals to bottom of page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const recipesUrl = "http://localhost:3000/recipes"
+const recipesUrl = "http://localhost:3000/recipes";
+const mealsUrl = "http://localhost:3000/meals";
 const pageSize = 5;
 let pageOffset = 0;
 const recipeBar = document.getElementById('recipe-bar');
@@ -41,4 +42,43 @@ function updatePage(value)
     }
     getRecipes();
 }
+
+//add meal plan functionality/feature
+const weeklyMealsButton = document.getElementById('weekly-meals-btn')
+weeklyMealsButton.addEventListener('click', toggleWeeklyMealsView);
+
+const mealPlanDiv = document.getElementById('meal-plan');
+
+function toggleWeeklyMealsView(event)
+{
+    //change text on button
+    weeklyMealsButton.innerHTML = weeklyMealsButton.innerHTML == 'Hide Weekly Meals' ? 'Weekly Meals' : 'Hide Weekly Meals';
+    //get div
+    mealPlanDiv.style = mealPlanDiv.style == 'display:show' ? 'display:none' : 'display:show';
+    console.log(mealPlanDiv.style);
+    //grab meals from database
+    fetch(mealsUrl)
+    .then(res=>res.json())
+    .then(addMealsToTable);
+}
+
+function addMealsToTable(meals)
+{
+    // meals.sort
+    mealPlanDiv.innerHTML = '';
+    mealPlanDiv.innerHTML = `<h4>Weekly Meals</h4>`;
+    meals.forEach(meal => addMeal(meal));
+}
+
+function addMeal(meal)
+{
+    const mealPlanCard = document.createElement('span');
+    mealPlanCard.innerHTML = `
+        <h4>${meal.date}</h4>
+        <p>${meal.typeOfMeal}</p>
+        <p>${meal.recipe_id}</p>
+    `
+    mealPlanDiv.appendChild(mealPlanCard);
+}
+
 
